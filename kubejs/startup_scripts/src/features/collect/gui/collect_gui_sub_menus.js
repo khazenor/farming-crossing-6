7967479bfd546ac6dfeca0422e2a14085e40b71e
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-06 22:35:07 -0600

    create and implement menu wrapper

--- kubejs/startup_scripts/src/features/collect/gui/collect_gui_sub_menus.js
+++ kubejs/startup_scripts/src/features/collect/gui/collect_gui_sub_menus.js
@@ -1,54 +1,26 @@
 const CollectGuiSubMenus = {
   openSubMenuForCategory (event, categoryId) {
-    let subMenu = new MenuType(CollectTransHelper.categoryName(categoryId))
+    let subMenu = new MenuWrapper(CollectTransHelper.categoryName(categoryId), { colStart: 1 })
     let subCollectionIdsByCollectionId = CollectCaches.subCollectionIdsByCollectionId
     let subCollectionIds = subCollectionIdsByCollectionId[categoryId]
-
-    let curPage = 0
-    let curRow = 1
-    let rowLength = 5
-    let colLength = 9
-    let colStart = 1
-    let rowStart = 0
     
     for (let subCollectionId of subCollectionIds) {
       let collectedIds = CollectLogger.playerCollectionByCategory(event, subCollectionId)
       let subCollectionItemIds = CollectCaches.categoryLists[subCollectionId]
       let itemIds = ArrayHelper.arrayDiff(subCollectionItemIds, collectedIds)
       subMenu.addSlot({
-        page: curPage,
-        x:0, y: curRow,
         label: CollectTransHelper.categoryName(subCollectionId),
         item: "minecraft:slime_ball"
-      })
-      let curCol = colStart
+      }, { "col": 0 })
       for (let itemId of itemIds) {
         subMenu.addSlot({
-          page: curPage,
-          x: curCol, y:curRow,
           label: TransHelper.itemName(itemId),
           item: itemId
         })
-        if (curCol < colLength - 1) {
-          curCol += 1
-        } else {
-          curCol = colStart
-          if (curRow < rowLength - 1) {
-            curRow += 1
-          } else {
-            curPage += 1
-            curRow = rowStart
-          }
-        }
       }
-      if (curRow < rowLength - 1) {
-        curRow += 1
-      } else {
-        curPage += 1
-        curRow = rowStart
-      }
+      subMenu.nextRow()
     }
 
-    subMenu.show(event.player)
+    subMenu.menu.show(event.player)
   }
 }
