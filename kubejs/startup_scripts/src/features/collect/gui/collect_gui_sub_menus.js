0ca6215e833e68b9d2088553c7860a76b3263d69
khazenor <80185732+khazenor@users.noreply.github.com>
2025-03-07 21:16:28 -0600

    only make subcollection menus if there are still items to collect

--- kubejs/startup_scripts/src/features/collect/gui/collect_gui_sub_menus.js
+++ kubejs/startup_scripts/src/features/collect/gui/collect_gui_sub_menus.js
@@ -8,26 +8,28 @@ const CollectGuiSubMenus = {
       let collectedIds = CollectLogger.playerCollectionByCategory(event, subCollectionId)
       let subCollectionItemIds = CollectCaches.categoryLists[subCollectionId]
       let itemIds = ArrayHelper.arrayDiff(subCollectionItemIds, collectedIds)
-      subMenu.addSlot({
-        label: CollectTransHelper.categoryName(subCollectionId),
-        item: "minecraft:nether_star"
-      }, { "col": 0 })
-      for (let itemId of itemIds) {
-        let itemIcon
-        let label
-        if (CollectEntity.isEntityCollection(categoryId)) {
-          itemIcon = CollectEntity.spawnEggFromEntityType(itemId)
-          label = TransHelper.entityName(itemId)
-        } else {
-          itemIcon = itemId
-          label = TransHelper.itemName(itemId)
-        }
+      if (itemIds.length > 0) {
         subMenu.addSlot({
-          label: label,
-          item: itemIcon
-        })
+          label: CollectTransHelper.categoryName(subCollectionId),
+          item: "minecraft:nether_star"
+        }, { "col": 0 })
+        for (let itemId of itemIds) {
+          let itemIcon
+          let label
+          if (CollectEntity.isEntityCollection(categoryId)) {
+            itemIcon = CollectEntity.spawnEggFromEntityType(itemId)
+            label = TransHelper.entityName(itemId)
+          } else {
+            itemIcon = itemId
+            label = TransHelper.itemName(itemId)
+          }
+          subMenu.addSlot({
+            label: label,
+            item: itemIcon
+          })
+        }
+        subMenu.nextRow()
       }
-      subMenu.nextRow()
     }
 
     subMenu.menu.show(event.player)
